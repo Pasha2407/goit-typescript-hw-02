d4f71c1367375aa252723fdb3a07a70e667696e5
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2023-12-09 23:16:36 +0200

    fixed 3.ts

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -1,6 +1,5 @@
-function merge<T, U>(objA: T, objB: U): Object {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
 
 export { }
-
